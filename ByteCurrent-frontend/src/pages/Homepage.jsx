6ba6c04d09cc1ef90918c7471509c48db71fe1bf
sleep478 <rayedtirmizi@gmail.com>
2025-04-19 12:04:00 +0300

    Update Homepage.jsx

--- ByteCurrent-frontend/src/pages/Homepage.jsx
+++ ByteCurrent-frontend/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import { Link } from 'react-router';
-import './Buttons.css'; 
+import '../../public/Buttons.css'; 
 import bgVideo from '/ByteCurrent-frontend/src/pages/unwrap_v43_2023-10-26_1332.mp4'; 
 function Homepage() {
   return (
